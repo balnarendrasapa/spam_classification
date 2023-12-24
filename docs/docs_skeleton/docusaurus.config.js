e7a56ac5468698cbd7bd920cc283b4e45136bead
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2023-12-23 19:06:16 -0500

    add default theme

--- docs/docs_skeleton/docusaurus.config.js
+++ docs/docs_skeleton/docusaurus.config.js
@@ -59,6 +59,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        // respectPrefersColorScheme: true,
+      },
       navbar: {
         title: "Spam Classification",
         logo: {
